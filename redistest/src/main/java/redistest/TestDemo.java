561f374b8d76380d9be060fae512973e0949ea0f
l-yd <lyd15944591059@outlook.com>
2019-12-20 18:06:09 +0800

    刘怡东修改

--- redistest/src/main/java/redistest/TestDemo.java
+++ redistest/src/main/java/redistest/TestDemo.java
@@ -5,7 +5,7 @@ public class TestDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("hello123");
-      System.out.println("nieyunshuai¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡££¬£¬£¬..123");
+      System.out.println("nieyunshuai¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡£¡££¬£¬£¬..123123");
       
 	}
 
